4e97f76eae3c39d328203df2d58f14f5abeef978
uphg <1834805770@qq.com>
2025-12-21 20:44:46 +0800

    chore: update version to 0.7.0 and adjust tsdown configuration

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   // entry: ['./src/index.ts'],
-  sourcemap: true,
-  clean: false,
   format: ['cjs', 'esm', 'umd'],
   outDir: './dist',
+  dts: true,
+  sourcemap: true,
+  clean: false,
   outputOptions: {
     name: 'xfunc'
   }
